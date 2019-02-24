f9c249839a5f884cbf4efe9c740dc5a8c8fb59b8
inux <inux.steve@gmail.com>
2019-02-24 20:40:23 +0100

    add note

--- MyHelloWorld/app/src/main/java/com/example/myhelloworld/MainActivity.java
+++ MyHelloWorld/app/src/main/java/com/example/myhelloworld/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.myhelloworld;
 
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.w("onCreate savedInstanceState", savedInstanceState);
-        super.onCreate(savedInstanceState);
-        Log.w("after calling super constructor", savedInstanceState);
+        super.onCreate(savedInstanceState); //savedInstanceState is null
         setContentView(R.layout.activity_main);
     }
 }
