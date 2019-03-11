144034e3c369ed7531fe7f0598ac6ac6e0976531
inux <inux.steve@gmail.com>
2019-03-11 19:46:46 +0100

    adding CommunicationAndConcurrency

---
+++ CommunicationAndConcurrency/app/src/main/java/com/example/communicationandconcurrency/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.commandconcurrency;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    void startCommHttpServer(View view) {
+
+    }
+
+    void startHttpDemos(View view) {
+
+    }
+
+    void startAsyncDemoTask(View view) {
+
+    }
+
+    void startDemoThread(View view) {
+
+    }
+
+    void blockUIforSevenSeconds(View view) {
+
+    }
+
+    void startAsyncMultiTask(View view) {
+
+    }
+}
