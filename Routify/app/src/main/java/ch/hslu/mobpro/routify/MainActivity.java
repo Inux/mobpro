2a8d1f87e9b692a7c9b434d8a2cea22533082097
inux <inux.steve@gmail.com>
2019-04-15 18:16:04 +0200

    add Routify project

---
+++ Routify/app/src/main/java/ch/hslu/mobpro/routify/MainActivity.java
@@ -0,0 +1,23 @@
+package ch.hslu.mobpro.routify;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * The main activity of Routify.
+ */
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
